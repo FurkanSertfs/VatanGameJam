430adb56ef94332910cb5c426a9fadf152f5b558
Eren Boran <erenn.borann@gmail.com>
2022-09-26 20:40:56 +0300

    CreditPanel

---
+++ Assets/OpenAboutPage.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenAboutPage : MonoBehaviour
+{
+    public GameObject CreditPanel;
+
+
+
+    public void OpenCredit()
+    {
+        CreditPanel.SetActive(true);
+
+    }
+}
